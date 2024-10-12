97ba65efe5f6644956a747c25f3bc0cacb5b1a12
Yash Zagekar <160099789+Yash-Zagekar@users.noreply.github.com>
2024-10-12 13:05:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def set_custom_css():
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.markdown("<h1 style='font-family: Poppins;'>📧 Business Mail Generator</h1>", unsafe_allow_html=True)
-    url_input = st.text_input("Enter a URL:", value="https://www.naukri.com/tata-communications-jobs")
+    url_input = st.text_input("Enter a URL:", value="https://hrmos.co/pages/moneyforward/jobs?category=1862070723159687168")
     submit_button = st.button("Submit")
 
     if submit_button:
